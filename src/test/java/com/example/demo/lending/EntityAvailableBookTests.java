ad3f51fdf031924c3764c36540064bd801aeac54
Yorreck Wagner <yorreck.wagner@outlook.de>
2024-08-15 19:52:08 +0200

    fix

--- src/test/java/com/example/demo/lending/EntityAvailableBookTests.java
+++ src/test/java/com/example/demo/lending/EntityAvailableBookTests.java
@@ -13,7 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 
 @SpringBootTest
-class EntityBookTests {
+class EntityAvailableBookTests {
         // Define the base package where your classes are located
         private static final String BASE_PACKAGE = "com.example.demo.lending";
 
@@ -21,7 +21,7 @@ class EntityBookTests {
         private static final String IDENTITY_NAME = "BookId";
 
         private static final String DOMAIN_LAYER = "..model..";
-        private static final String BOUDNED_CONTEXT = "..lending..";
+        private static final String BOUNDED_CONTEXT = "..lending..";
 
         @Test
         void moduleShouldContainClassWithEntitiesName() {
@@ -59,7 +59,7 @@ class EntityBookTests {
                                 .filter(javaClass -> javaClass.getSimpleName().equals(ENTITY_NAME)).findFirst().get();
 
                 ArchRule rule = classes().that().haveFullyQualifiedName(entityClass.getFullName()).should()
-                                .resideInAPackage(BOUDNED_CONTEXT);
+                                .resideInAPackage(BOUNDED_CONTEXT);
 
                 rule.check(importedClasses);
         }
